9e8cd4f7812a2c1a250b3c8935e1120f1f57fda0
Paweł Pluta <p.pluta.86@gmail.com>
2017-12-13 19:03:33 +0100

    VideoGrabber refactored, gui populated with 2 Sliders

--- src/air_painter/Controller.java
+++ src/air_painter/Controller.java
@@ -1,11 +1,13 @@
 package air_painter;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class Controller {
 
-    public Label introMessage;
+    @FXML
+    private Label introMessage;
 
     public void printIntroInfo(ActionEvent actionEvent) {
         introMessage.setText("Start !");
--- src/air_painter/VideoGrabber.java
+++ src/air_painter/VideoGrabber.java
@@ -2,8 +2,18 @@ package air_painter;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.Videoio;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by Pawel Pluta on 12/12/17.
@@ -11,10 +21,13 @@ import org.opencv.videoio.Videoio;
 public class VideoGrabber {
 
     private VideoCapture camera = null;
-    private Mat frame = new Mat();
 
-    public void loadOpenCVLibrary() {
+    public static void loadOpenCVLibrary() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    public boolean isCameraRunning() {
+        return camera.isOpened();
     }
 
     public void initializeCamera(int cameraNumber) {
@@ -24,13 +37,33 @@ public class VideoGrabber {
         camera.set(Videoio.CV_CAP_PROP_BRIGHTNESS, 0.3);
     }
 
-    public boolean isCameraRunning() {
-        return camera.isOpened();
+    public Mat getNextFrame() {
+        Mat frame = new Mat();
+        if(isCameraRunning()) {
+            camera.read(frame);
+        }
+        return frame;
     }
 
-    public Mat getNextFrame() {
-        camera.read(frame);
-        return frame;
+    public BufferedImage getNextFrameAsImage() {
+        Mat frame = getNextFrame();
+        InputStream stream = convertFrameToStream(frame);
+        return getImageFromStream(stream);
+    }
+
+    private InputStream convertFrameToStream(Mat frame) {
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".bmp", frame, buffer);
+        return new ByteArrayInputStream(buffer.toArray());
+    }
+
+    private BufferedImage getImageFromStream(InputStream stream) {
+        try {
+            return ImageIO.read(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
